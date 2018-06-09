5347ab4065e7d9e9e7adbdb4468bd4d5371b9228
CrazyGuy108 <taylor@hansenfamily.us>
2018-06-08 18:41:10 -0700

    Animate fireball

--- fireball/fireball.gd
+++ fireball/fireball.gd
@@ -1,15 +1,16 @@
 extends Area2D
 
+onready var max_frames = $Sprite.hframes * $Sprite.vframes
+
 # speed in pixels/second, must be set during initialization
-var speed
+var speed = 0
+
+func _ready():
+    $Sprite
 
 func _process(delta):
     # move along the current direction
     move_local_x(speed * delta, true)
-    
-    # fireball dies if time is up
-    if $Timer.is_stopped():
-        queue_free()
 
 func _on_area_entered(area):
     """
@@ -18,6 +19,16 @@ func _on_area_entered(area):
     # see if we killed an enemy with fire
     if area.is_in_group("enemies"):
         area.emit_signal("fired")
-        
-        # destroy the fireball
         queue_free()
+
+func _on_Timer_timeout():
+    """
+    Called once the fireball's Timer runs out.
+    """
+    queue_free()
+
+func _on_AnimationTimer_timeout():
+    """
+    Called when it's time to switch to the next animation frame
+    """
+    $Sprite.frame = ($Sprite.frame + 1) % max_frames
